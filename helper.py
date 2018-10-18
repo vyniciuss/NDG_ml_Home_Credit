f1b322fe5572976c84a5a814e376d4bf35618aa8
vyniciuss <vyniciuss@gmail.com>
2018-10-18 00:28:29 -0300

    iniciando benchmark

--- helper.py
+++ helper.py
@@ -9,19 +9,19 @@ from plotly import tools
 init_notebook_mode(connected=True)
 import warnings
 
-def draw_bar_plot_vert(col_name, data):    
-    tempAll = data[col_name].value_counts()
-    temp1 = data[data["TARGET"] == 1][col_name].value_counts()
-    temp0 = data[data["TARGET"] == 0][col_name].value_counts()
+def draw_bar_plot_vert(col_name, df):    
+    tempAll = df[col_name].value_counts()
+    temp1 = df[data["TARGET"] == 1][col_name].value_counts()
+    temp0 = df[data["TARGET"] == 0][col_name].value_counts()
     
     trace0 = go.Bar(x=tempAll.index, y=(tempAll / tempAll.sum())*100, name='All', marker=dict(color="#49b675"))
     trace1 = go.Bar(x=temp1.index, y=(temp1 / temp1.sum())*100, name='Target = 1', marker=dict(color="#e61919"))
     trace2 = go.Bar(x=temp0.index, y=(temp0 / temp0.sum())*100, name='Target = 0', marker=dict(color="#87cefa"))
     return trace0, trace1, trace2 
 
-def missing_values(data):
-    total = data.isnull().sum().sort_values(ascending = False)
-    percentual = (data.isnull().sum()/data.isnull().count()*100).sort_values(ascending = False).round(1)
+def missing_values(df):
+    total = df.isnull().sum().sort_values(ascending = False)
+    percentual = (df.isnull().sum()/df.isnull().count()*100).sort_values(ascending = False).round(1)
     missing_data  = pd.concat([total, percentual], axis=1, keys=['Total de Ausentes', 'Percentual de Ausentes'])
     return missing_data
 
@@ -31,3 +31,13 @@ def print_percentuals_default(df, column, ruido):
     print('Registros diferentes do valor informado cometeram calote em %0.2f%% dos empréstimos' % (100 * no_outlier['TARGET'].mean()))
     print('Registros iguais ao valor informado cometeram calote em  %0.2f%% dos empréstimos' % (100 * outlier['TARGET'].mean()))
     print('Existem {} valores iguais ao informado "{}" no conjunto de dados'.format(len(outlier), ruido))
+    
+def draw_count_uniques(col_name, df, titulo):
+    temp = df[col_name].astype('category').map({0:'NÃO',1:'SIM'})
+    count_uniques = temp.value_counts()
+    data = [go.Bar(x=count_uniques.index, y=(count_uniques / count_uniques.sum())*100, name='All', marker=dict(color=['rgb(49,130,189)', 'rgb(204,204,204)']))]
+    layout = go.Layout(title=titulo)
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig)
+    print("Total de itens em cada grupo:")
+    print(temp.value_counts())
